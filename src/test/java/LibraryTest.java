f67e8d2d8091ea6cdc57fbbdb12c86bc18bcb7ae
Niomahony <93268576+Niomahony@users.noreply.github.com>
2022-11-09 16:29:49 +0000

    Update LibraryTest.java

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -11,5 +11,39 @@ class LibraryTest {
         historyDept.rentBook(a);
         compSciDept.rentBook(a);
         assertFalse(compSciDept.getCurrentRentedBooks().contains(a));
+        
+    //Testing for unnecessary subscriptions to journals with limited interest || access is available via external universities 
+    @Test
+    void subscriptionTest1(){
+        Journal j = new Journal("a", "b");
+        j.setBorrowTotal(0); // Borrow total below limit
+        j.setExternalAccess(true);  // Another Uni has access to the journal
+        assertTrue(j.isSubscriptionStatus());
+    }
+
+    @Test
+    void subscriptionTest2(){
+        Journal j = new Journal("1", "2");
+        j.setBorrowTotal(10); // Borrow total above limit
+        j.setExternalAccess(false);  // Another Uni does not have access to the journal
+        assertTrue(j.isSubscriptionStatus());
+    }
+
+    @Test
+    void subscriptionTest3(){
+        Journal j = new Journal("Yeet", "Skrrt");
+        j.setBorrowTotal(0); // Borrow total below limit
+        j.setExternalAccess(false);  // Another Uni does not have access to the journal
+        assertTrue(j.isSubscriptionStatus());
+    }
+
+    @Test
+    void subscriptionTest4(){
+        Journal j = new Journal("Beep", "Boop");
+        j.setBorrowTotal(10); // Borrow total below limit
+        j.setExternalAccess(true);  // Another Uni has access to the journal
+        assertTrue(j.isSubscriptionStatus());
+    }
+        
     }
 }
