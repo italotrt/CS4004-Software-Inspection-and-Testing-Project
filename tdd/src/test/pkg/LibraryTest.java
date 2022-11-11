9caf90c6d19f554e1126a6fb58e080c879d60dad
Niomahony <93268576+Niomahony@users.noreply.github.com>
2022-11-11 13:33:23 +0000

    Test For department book budget
    poggers

--- tdd/src/test/pkg/LibraryTest.java
+++ tdd/src/test/pkg/LibraryTest.java
@@ -158,5 +158,13 @@ import static org.junit.jupiter.api.Assertions.*;
             }, "Book not found!");
 
         }
+        @Test
+        void testBudgetRestrictions(){
+            Department d = new Department("D1");
+            d.setBudget(8000);
+            Book b =  new Book("Beans", "Bean Eating", "Bean University", "Bean department");
+            b.setPrice(100);
+            assertTrue(d.bookBudgetCheck(d.getBudget(), d.purchaseBooks(b, 60)));
+        }
 
         }
