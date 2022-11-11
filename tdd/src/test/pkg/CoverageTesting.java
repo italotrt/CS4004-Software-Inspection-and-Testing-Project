c14a1d98a39e58d5a2932aa6d373c8cb43ff5033
Italo Terto <italoterto4@gmail.com>
2022-11-11 17:36:51 +0000

    Started Coverage Criteria of Department class

--- tdd/src/test/pkg/CoverageTesting.java
+++ tdd/src/test/pkg/CoverageTesting.java
@@ -2,25 +2,35 @@ package pkg;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CoverageTesting {
 
-    static Book a;
-    static Library b;
+    static Book aBook;
+    static Book bBook;
+    static Book cBook;
+    static Library lib;
+    static Department compSci;
 
     @BeforeAll
     public static void setUp() {
-        a = new Book("test", "maths", 2, "A1");
-        b = new Library(true);
+        aBook = new Book("test", "maths", 2, "A1");
+        bBook = new Book("BompSci", "maths", 2, "A1");
+        cBook = new Book("CompSci", "maths", 2, "A1");
+        lib = new Library(true);
+        compSci = new Department("compSciDept");
     }
 
     @Test
     void testSearchBook() {
-        b.addBook(a);
+        lib.addBook(aBook);
         Book nonExist = new Book("no", "no", 2, "A1");
-        assertTrue(b.searchBook(a));
-        assertFalse(b.searchBook(nonExist));
+        assertTrue(lib.searchBook(aBook));
+        assertFalse(lib.searchBook(nonExist));
     }
 
     @Test
@@ -29,5 +39,22 @@ public class CoverageTesting {
         assertFalse(newLib.isOpen());
         newLib.setOpen(true);
         assertTrue(newLib.isOpen());
+    }
+
+    @Test
+    void testDepartmentConstructor() {
+        assertInstanceOf(Department.class, compSci);
+    }
+
+    @Test
+    void testRentBook() {
+        compSci.rentBook(aBook);
+        Book[] books = new Book[]{aBook, bBook, cBook};
+        cBook.setAvailable(false);
+
+        assertAll(() -> assertTrue(compSci.getCurrentRentedBooks().contains(books[0])),
+                  () -> assertFalse(compSci.getCurrentRentedBooks().contains(books[1])),
+                  () -> assertLinesMatch((Stream<String>) compSci.getDepartmentBooks(), (Stream<String>) compSci.getCurrentRentedBooks())
+        );
     }
 }
